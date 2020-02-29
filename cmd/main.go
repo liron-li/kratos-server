8949ee6e6ea13e034a344edc397a045acce6de4d
liron-li <liron.li@outlook.com>
2020-02-29 17:39:02 +0800

    服务å注册

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"github.com/bilibili/kratos/pkg/conf/env"
+	"github.com/bilibili/kratos/pkg/conf/paladin"
+	"github.com/bilibili/kratos/pkg/log"
+	"github.com/bilibili/kratos/pkg/naming"
+	"github.com/bilibili/kratos/pkg/naming/discovery"
+	"kratos-server/internal/di"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	"kratos-server/internal/di"
-	"github.com/bilibili/kratos/pkg/conf/paladin"
-	"github.com/bilibili/kratos/pkg/log"
 )
+
+func registerWarden() context.CancelFunc {
+
+	ip := "127.0.0.1"   // NOTE: 必须拿到您实例节点的真实IP，
+	port := "9000" // NOTE: 必须拿到您实例grpc监听的真实端口，warden默认监听9000
+	hn, _ := os.Hostname()
+	dis := discovery.New(nil)
+	ins := &naming.Instance{
+		Zone:     env.Zone,
+		Env:      env.DeployEnv,
+		AppID:    "server.service",
+		Hostname: hn,
+		Addrs: []string{
+			"grpc://" + ip + ":" + port,
+		},
+	}
+	cancel, err := dis.Register(context.Background(), ins)
+	if err != nil {
+		panic(err)
+	}
+
+	// 省略...
+
+	// 特别注意！！！
+	// cancel必须在进程退出时执行！！！
+	return cancel
+}
 
 func main() {
 	flag.Parse()
@@ -22,6 +52,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	cancel := registerWarden()
+
+	defer cancel()
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
 	for {
--- internal/server/http/server.go
+++ internal/server/http/server.go
@@ -10,10 +10,10 @@ import (
 	bm "github.com/bilibili/kratos/pkg/net/http/blademaster"
 )
 
-var svc pb.DemoServer
+var svc pb.ServerServer
 
 // New new a bm server.
-func New(s pb.DemoServer) (engine *bm.Engine, err error) {
+func New(s pb.ServerServer) (engine *bm.Engine, err error) {
 	var (
 		cfg bm.ServerConfig
 		ct paladin.TOML
@@ -26,7 +26,7 @@ func New(s pb.DemoServer) (engine *bm.Engine, err error) {
 	}
 	svc = s
 	engine = bm.DefaultServer(&cfg)
-	pb.RegisterDemoBMServer(engine, s)
+	pb.RegisterServerBMServer(engine, s)
 	initRouter(engine)
 	err = engine.Start()
 	return
